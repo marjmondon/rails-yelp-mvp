908ae0eb42c88591efb42ccb9973df729a62fd51
Marjorie MONDON <marjmondon@hotmail.com>
2022-11-23 11:52:47 -0500

    models ok

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+end
